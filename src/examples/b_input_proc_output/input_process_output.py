11f485f20c131f58e154aaf085bd7d1b626a5c7a
Jrice176 <jackson.rice@g.austincc.edu>
2025-02-06 01:35:34 +0000

    w

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,25 @@ def display_output():
 def test_config():
     return True
 
+def use_int_type():
+    num = 10
+    print(num)
+    num = num + 5
+    print(num)
+    num = "Python"
+    print(num)
+
+def use_float_type():
+    num = 10.99
+    print(num)   
+
+def use_string_type():
+    lang = Python
+    print(lang)
+
+    lang = 10
+    print(lang)
+
+    lang = 10.99
+    print(lang)
+
