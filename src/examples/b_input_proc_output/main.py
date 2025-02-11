ef19747acc4fc1e0f99d75ff62d68a5be4c8194b
Jrice176 <jackson.rice@g.austincc.edu>
2025-02-11 01:51:45 +0000

    test and,or,not truth tables

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -4,6 +4,8 @@ def main():
     num1 = input("Enter a number: ")
     num2 = input("Enter a number: ")
 
+    print(num1)
+    
     result = input_process_output.add_numbers(int(num1),int(num2))
     print(result)
 
