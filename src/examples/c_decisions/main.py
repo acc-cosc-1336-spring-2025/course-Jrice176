788989921f31382bc3fe497b64e01fff378e74a9
Jrice176 <jackson.rice@g.austincc.edu>
2025-02-11 02:05:10 +0000

    w

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -4,6 +4,7 @@ def main():
     num1 = input("Enter number:)")
     num2 = input("Enter number: ")
 
+#hhd
     result = decisions.compare_numbers_equality(int(num1), int(num2))
 
     print(result)
