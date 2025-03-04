4d281d8656af504fdab5be9412f6156a3182b9d6
Jrice176 <jackson.rice@g.austincc.edu>
2025-03-04 01:13:51 +0000

    string concatenation

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,6 +1,7 @@
 #main program
 def main():
     lang = 'python'
-    print(lang[0]) #-1 #string slicing
-    print(len(lang))
+    print(lang)
+    lang = lang + ' is great'
+    print(lang)
 main()
