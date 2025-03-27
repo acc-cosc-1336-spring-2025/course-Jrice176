be936cf09d6f747c16c5aacc315b392b9b8ec35b
Jrice176 <jackson.rice@g.austincc.edu>
2025-03-27 00:47:44 +0000

    lists

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,11 @@
 def test_config():
     return True
+
+def list_as_function_parameter(list1):
+    list1[0] = 100
+
+def display_multiplication_table(list):
+    for row_list in list:
+        for item in row_list:
+            print(str(item).rjust(3," "), end = " ")
+        print(" ")
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,6 @@
 #main program
+import lists
+def main():
+    list1 = lists.get_multiplication_table(10, 10)
+    lists.display_multiplication_table(list1)
+main()
