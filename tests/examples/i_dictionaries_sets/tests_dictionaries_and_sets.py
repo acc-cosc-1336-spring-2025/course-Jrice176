52d33509136aee4f82563fac9be1f16e0fc31a84
Jrice176 <jackson.rice@g.austincc.edu>
2025-04-01 00:33:15 +0000

    in and not in diction.

--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -8,8 +8,20 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(True, test_config())
 
     def test_access_dictionary_value(self):
-        phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+        phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333', 'Sam':'555-4444'}
         self.assertEqual(phone_book['Chris'], '555-1111')
         self.assertEqual(phone_book['Katie'], '555-2222')
         self.assertEqual(phone_book['Joanne'], '555-3333')
+        self.assertEqual(phone_book['Sam'], '555-4444')
 
+    def test_key_in_dictionary(self):
+        phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+        self.assertEqual('Katie' in phone_book, True)
+        self.assertEqual('Sam' in phone_book, False)
+
+    def test_key_not_in_dictionary(self):
+        phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+        self.assertEqual('Sam'not in phone_book, True)
+        self.assertEqual('Katie' not in phone_book, False)
+
+    
