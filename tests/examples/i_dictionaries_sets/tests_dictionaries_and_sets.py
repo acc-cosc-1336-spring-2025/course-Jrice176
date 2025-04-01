67b7ad705ede3b2ad5f5486541d12c5e53ed450b
Jrice176 <jackson.rice@g.austincc.edu>
2025-04-01 00:49:54 +0000

    mixing up values

--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -29,8 +29,27 @@ class Test_Config(unittest.TestCase):
         if 'Sam' not in phone_book:
             phone_book['Sam'] = '555-4444'
         self.assertEqual(phone_book['Sam'], '555-4444')
+    
     def test_delete_key_value_pair_dictionary(self):
         phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
         if 'Chris' in phone_book:
             del phone_book['Chris']
         self.assertEqual('Chris' not in phone_book, True)
+    
+    def test_get_number_of_elements_dictionary(self):
+        phone_book = {'Chris':'555-1111', 'Katie':'555-2222', 'Joanne':'555-3333'}
+        self.assertEqual(len(phone_book), 3)
+
+    def test_dictionary_supports_different_data_value_types(self):
+        phone_book = {'Chris':5551111, 'Katie':[555-2222], 'Joanne':'555-3333'}
+        self.assertEqual(phone_book['Chris'], 5551111)
+        self.assertEqual(phone_book['Katie'], [555-2222])
+        self.assertEqual(phone_book['Joanne'], '555-3333')
+    
+    def test_dictionary_supports_data_key_types(self):
+        phone_book = {555-1111:'Chris', 'Katie':'555-2222', 50.5: 'Joanne'}
+        self.assertEqual(phone_book['Katie'], '555-2222')
+        self.assertEqual(phone_book[555-1111], 'Chris')
+        self.assertEqual(phone_book[50.5], 'Joanne')
+    
+    
