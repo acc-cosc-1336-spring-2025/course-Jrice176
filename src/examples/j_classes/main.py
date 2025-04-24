4f4dce012b9d31ae518b1b27b79a18a71b9e046b
Jrice176 <jackson.rice@g.austincc.edu>
2025-04-24 00:49:54 +0000

    deposit withdraw main

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -4,7 +4,10 @@ from bank_account import BankAccount
 def main():
     account1 = BankAccount(500)#variable represents a BankAccount --- object or instance of class
     print(account1.get_balance())
-    amount = input("Enter amount to deposit: ")
+    amount = input("Enter amount to deposit:")
     account1.deposit(int(amount))
+    print(account1.get_balance())
+    amount = input("Enter amount to withdraw:")
+    account1.withdraw(int(amount))
     print(account1.get_balance())
 main()
